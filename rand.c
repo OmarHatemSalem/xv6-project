235c94747045e81e5de02a8cb6e11ca9f64fe7bf
OmarHatemSalem <omarhatem2002@aucegypt.edu>
2023-03-06 22:36:50 +0200

    adds validation on bounds

--- rand.c
+++ rand.c
@@ -15,26 +15,31 @@ int CalcDays(int iMonth, int iDay, int iYear)
 int
 main(int argc, char *argv[])
 {
-  struct rtcdate dt;
-  // getali()
-  date(&dt);
+    struct rtcdate dt;
+    // getali()
+    date(&dt);
 
-  if (argc < 3)
-  {
+    if (argc < 3)
+    {
     printf(2,"Insufficient Funds... \n");
     exit();
     /* code */
-  }
-  int lowerBound = atoi(argv[1]);
-  int upperBound = atoi(argv[2]);
+    }
+    int lowerBound = atoi(argv[1]);
+    int upperBound = atoi(argv[2]);
 
-  int seed = dt.year + dt.day+ dt.month + dt.hour + dt.minute + dt.second;
- int rand = ((seed * 214013 + 2531011) >> 16) & 0x7fff; 
- int rand2 = ((rand * 214013 + 2531011) >> 16) & 0x7fff;
-printf(1,"Random Number: %d\n",rand2 % (upperBound - lowerBound) + lowerBound);
-  
-  
-  exit();
+    if (lowerBound > upperBound) {
+        printf(2, "Lower Bound cannot be greater than upper bound.\n");
+        exit();
+    }
+
+    int seed = dt.year + dt.day+ dt.month + dt.hour + dt.minute + dt.second;
+    int rand = ((seed * 214013 + 2531011) >> 16) & 0x7fff; 
+    int rand2 = ((rand * 214013 + 2531011) >> 16) & 0x7fff;
+    printf(1,"Random Number: %d\n",rand2 % (upperBound - lowerBound) + lowerBound);
+
+
+    exit();
 }
 
 
